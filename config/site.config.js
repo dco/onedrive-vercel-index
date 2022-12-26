187d7f5502aabc1fbf5b71d4a9d537eabbb9a519
dco <uiqian@live.cn>
2022-12-26 15:46:32 +0800

    delete public

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/public/Secret', '/public/Secret2/'],
+  protectedRoutes: ['/Secret', '/Secret2/'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
