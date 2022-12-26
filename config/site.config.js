373a23c766c079a33b78de0206e3457835c5f94c
dco <uiqian@live.cn>
2022-12-26 14:19:58 +0800

    Home

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'github',
+      name: 'Home',
       link: 'https://dcocd.com/',
     },
   ],
