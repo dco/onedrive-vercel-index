104c37e10adf6d77d62849fefa40a9cf7693c2a6
dco <uiqian@live.cn>
2022-12-26 14:05:37 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'House',
+      name: 'github',
       link: 'https://dcocd.com/',
     },
   ],
